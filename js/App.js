ac852add265565ebb63129e3dbb184c21a3f63cb
WalidAlrefai <w.alrefai.3@gmail.com>
2022-01-22 10:13:42 +0200

    New Js File

---
+++ js/App.js
@@ -0,0 +1,26 @@
+// "use strict";
+// prompt("Please Enter your Name:");
+// var userGender=prompt("Plrease Enter Your gender")
+// if(userGender.toLocaleLowerCase === "male"){
+//     console.log("male")
+// }
+var yourName =prompt("Please enter your Name");
+var yourGender = prompt("What is your gender");
+var yourAge = prompt("What is your age");
+
+if(yourAge <= 0){
+    alert("Your age is wrong")
+}
+
+var welcomingMassage=confirm( "Do you want to skip the welcoming massages?!")
+if(!welcomingMassage){
+    if(yourGender.toLocaleLowerCase()=="male"){
+        alert("Welcom Mr " + yourName);
+    }else if(yourGender.toLocaleLowerCase() == "female"){
+        alert("Welcom Ms "+ yourName);
+    }else{
+        alert("Welcome " + yourName)
+    }
+}else{
+    console.log("User skipped the welcom massage")
+}
